db19d8fcd90115ae7f7d3eb7e3cb66e43a4d33b7
Jhonathan <guarino90@gmail.com>
2023-02-08 00:04:01 -0300

    :tada: Create the card componnent
    Create the card component
    .

---
+++ src/components/CardGame/index.js
@@ -0,0 +1,14 @@
+import "./style.css";
+
+function CardGame() {
+  return `
+    <article class="card-game">
+        <img src= "images/pokemon-card.png" alt = "Carta Pokemon"> 
+    </article>
+    `;
+}
+
+// Exportar para o lado de fora do diretório //
+export default CardGame;
+
+// return: Ao executar a função, irá retornar o oque está escrito nela, ou seja o html que nós escrevemos entre as cráses //
